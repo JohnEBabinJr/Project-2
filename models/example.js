1da55e016d39c8dc70f98aed35bf2c01960766e5
Connor Paxton <51467803+Hippopattymus@users.noreply.github.com>
2019-09-25 20:33:52 -0700

    this doesnt exist

--- models/example.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (sequelize, DataTypes) {
-  var bathrooms = sequelize.define("bathrooms", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-  return bathrooms;
-};
